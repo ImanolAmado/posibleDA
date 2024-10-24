d3761db523ed2dc601468a8636d3ac297f61d4e5
Imanol <imanolamado@hotmail.com>
2024-10-24 08:52:44 +0200

    commit segundo

---
+++ controllers/controller_anadirLibro.php
@@ -0,0 +1,14 @@
+
+    <?php
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Recibir datos de la solicitud POST
+    $input = json_decode(file_get_contents('php://input'), false);
+
+    
+    var_dump($input);   
+}
+    
+    
+?>
